2387a18f023513bacbee9ccaef7f6fc2d20c2c4a
ihsakamill <ihsamuhamar986@gmail.com>
2024-07-17 12:41:51 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function tampilkanPesan() {
+    const message = document.getElementById('message');
+    message.innerHTML = `
+        haii sayang good morning, i just woke up, don't forget to eat ur lunch, take care always and stay hydrated. always remember that i love u so much
+        aku mau minta maaf karna kelakuan aku selama deket sama kamu, i hope kamu ga ilfeel ya and kamu mau maafin aku, maaf aku sering cemburu huhu, aku gatau gimana caranya ilangin itu
+        maaf tadi malem kita berantem gara gara salah paham dari aku, kamu pasti happy kan ws? hehe. kamu masih suka or sayang sama aku vel? coba di jawab jujur yaa
+        huu buat ginian aku kira susah HAHAH canda
+    `;
+}
